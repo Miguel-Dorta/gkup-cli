bb67c3c34896428e0d3ac3849720e21f2bd23ed9
Miguel-Dorta <contact@migueldorta.com>
2020-05-01 11:57:42 +0100

    Fix bug install scripts

--- install/linux_install.sh
+++ install/linux_install.sh
@@ -2,6 +2,7 @@
 TMP_DIR="/tmp/Miguel-Dorta_gkup_install"
 INSTALL_DIR="/opt/Miguel-Dorta/gkup"
 ARCH="$(uname -m)"
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 case $ARCH in
 x86_64)
@@ -14,13 +15,12 @@ esac
 
 # Download binaries
 mkdir $TMP_DIR || exit 1
-cd $TMP_DIR || exit 1
-wget -o gkup-core "https://github.com/Miguel-Dorta/gkup-core/releases/latest/download/gkup-core_$(uname -s)_$ARCH" || exit 1
-wget -o gkup-cli "https://github.com/Miguel-Dorta/gkup-cli/releases/latest/download/gkup-cli_$(uname -s)_$ARCH" || exit 1
+wget -qO- "https://github.com/Miguel-Dorta/gkup-cli/releases/latest/download/gkup_${OS}_${ARCH}.gz" | gzip -d > $TMP_DIR/gkup-cli || exit 1
+wget -qO- "https://github.com/Miguel-Dorta/gkup-core/releases/latest/download/gkup-core_${OS}_${ARCH}.gz" | gzip -d > $TMP_DIR/gkup-core || exit 1
 
 # Put binaries
-sudo mkdir $INSTALL_DIR || exit 1
-sudo mv gkup-core gkup-cli $INSTALL_DIR || exit 1
+sudo mkdir -p $INSTALL_DIR || exit 1
+sudo mv $TMP_DIR/gkup-core $TMP_DIR/gkup-cli $INSTALL_DIR || exit 1
 sudo ln -s $INSTALL_DIR/gkup-cli $INSTALL_DIR/gkup
 sudo chown -R root:root $INSTALL_DIR || exit 1
 sudo chmod -R 0755 $INSTALL_DIR || exit 1
@@ -29,5 +29,6 @@ sudo chmod -R 0755 $INSTALL_DIR || exit 1
 rm -Rf $TMP_DIR
 
 # Set env vars and path
-sudo bash -c 'echo -e "export GKUP_PATH=$INSTALL_DIR/gkup-core\nexport PATH=$PATH:$INSTALL_DIR" > /etc/profile.d/gkup.sh'
+gkupPath="$INSTALL_DIR/gkup-core"
+sudo bash -c "echo -e 'export GKUP_PATH=$INSTALL_DIR/gkup-core\nexport PATH=\$PATH:$INSTALL_DIR' > /etc/profile.d/gkup.sh"
 sudo chmod 0755 $INSTALL_DIR/gkup-core
--- install/linux_uninstall.sh
+++ install/linux_uninstall.sh
@@ -1,1 +1,1 @@
-rm -R /opt/Miguel-Dorta/gkup /etc/profile.d/gkup.sh
+sudo rm -R /opt/Miguel-Dorta/gkup /etc/profile.d/gkup.sh
